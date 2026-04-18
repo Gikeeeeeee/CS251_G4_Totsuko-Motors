b7fdaa037572dba0781efdc59d2ff08bea2f2334
jamespramkaew <jamespramkaew88@gmail.com>
2026-04-18 18:18:20 +0700

    Modified Sidebar and Create TopNav

--- frontend/src/app/technician/request-list/page.tsx
+++ frontend/src/app/technician/request-list/page.tsx
@@ -3,16 +3,20 @@
 import { useState } from 'react'
 // import SidebarClerk from '@/components/SidebarClerk'
 import Sidebar from '@/components/Sidebar';
-import TopNavClerk from '@/components/TopNavClerk'
+// import TopNavClerk from '@/components/TopNavClerk'
+import TopNav from '@/components/TopNav';
 
 export default function TechnicianPage() {
     const [requests, setRequests] = useState([]);
 
     return (
-        <div>
+        <div className='flex bg-[#F3FAFF]'>
             <Sidebar />
-            <div>
-                <TopNavClerk />
+            <div className='flex flex-col w-full'>
+                <TopNav />
+                <div className='flex p-[32px]'>
+                    <p className='text-[32px] font-bold text-[#002446]'>Request & Checking</p>
+                </div>
             </div>
 
         </div>
