ae638ae89c841f19f843312210ec254e0a95a3fe
Thanapapop <thanapapop.sri@dome.tu.ac.th>
2026-04-23 22:55:59 +0700

    fix: merge subfolder into main repo.

--- CS251_G4_Totsuko-Motors/backend/src/routes/parts/parts.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getPurchasingPartsController } from '../../controllers/parts/purchasing-parts.controller';
-
-const router = Router();
-
-router.get('/purchasing', getPurchasingPartsController);
-
-export default router;
