bcca9ae7091f59c78aafdde41e3e92dcbe2777c5
Meammi <bhoriwatn@gmail.com>
2026-04-28 21:01:54 +0700

    API get serviceJob by requestId

--- backend/src/routes/service/service.routes.ts
+++ backend/src/routes/service/service.routes.ts
@@ -3,6 +3,7 @@ import { getServiceRequests } from '../../controllers/service/clerk-dashboard.co
 import { createUsePart } from '../../controllers/service/part-usage.controller';
 import {
   createServiceJob,
+  getServiceJob,
   updateServiceJob,
 } from '../../controllers/service/service-job.controller';
 import { createServiceController } from '../../controllers/service/service-request.controller';
@@ -12,6 +13,7 @@ const router = Router();
 router.get('/service-request', getServiceRequests);
 router.post('/service-request/create', createServiceController);
 router.post('/:serviceId/service-job', createServiceJob);
+router.get('/service-job/:id', getServiceJob);
 router.put('/service-job/:id', updateServiceJob);
 router.post('/service-job/:id/parts', createUsePart);
 
